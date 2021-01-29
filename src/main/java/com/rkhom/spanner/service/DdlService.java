11e81ddecc2614cafee5aae3e50045d28d9e2ec6
rkhom <khom.rv@gmail.com>
2021-01-29 18:41:49 +0200

    Added embedded entities filtration in DDL

--- src/main/java/com/rkhom/spanner/service/DdlService.java
+++ src/main/java/com/rkhom/spanner/service/DdlService.java
@@ -2,6 +2,7 @@ package com.rkhom.spanner.service;
 
 import com.google.cloud.spring.data.spanner.core.admin.SpannerDatabaseAdminTemplate;
 import com.google.cloud.spring.data.spanner.core.admin.SpannerSchemaUtils;
+import com.google.cloud.spring.data.spanner.core.mapping.Embedded;
 import com.google.cloud.spring.data.spanner.core.mapping.Interleaved;
 import com.google.cloud.spring.data.spanner.core.mapping.SpannerMappingContext;
 import com.google.cloud.spring.data.spanner.core.mapping.SpannerPersistentEntity;
@@ -31,45 +32,71 @@ public class DdlService {
   private final SpannerMappingContext spannerMappingContext;
 
   public void createTables() {
-    Set<String> interleavedTables = getInterleavedTableNames();
+    Set<Class<?>> interleavedEntities = getInterleavedEntityClasses();
+    Set<Class<?>> embeddedEntities = getEmbeddedEntityClasses();
 
     List<String> creationDdls = spannerMappingContext.getPersistentEntities().stream()
-        .filter(entity -> !interleavedTables.contains(entity.tableName()))
+        .filter(entity -> !interleavedEntities.contains(entity.getType()))
+        .filter(entity -> !embeddedEntities.contains(entity.getType()))
         .filter(entity -> !spannerDatabaseAdminTemplate.tableExists(entity.tableName()))
         .map(PersistentEntity::getType)
         .map(spannerSchemaUtils::getCreateTableDdlStringsForInterleavedHierarchy)
         .flatMap(Collection::stream)
         .collect(Collectors.toList());
 
+    if (creationDdls.isEmpty()) {
+      return;
+    }
+
     spannerDatabaseAdminTemplate.executeDdlStrings(creationDdls, true);
   }
 
   public void dropTables() {
-    Set<String> interleavedTables = getInterleavedTableNames();
+    Set<Class<?>> interleavedEntities = getInterleavedEntityClasses();
 
     List<String> droppingDdls = spannerMappingContext.getPersistentEntities().stream()
-        .filter(entity -> !interleavedTables.contains(entity.tableName()))
+        .filter(entity -> !interleavedEntities.contains(entity.getType()))
         .filter(entity -> spannerDatabaseAdminTemplate.tableExists(entity.tableName()))
         .map(PersistentEntity::getType)
         .map(spannerSchemaUtils::getDropTableDdlStringsForInterleavedHierarchy)
         .flatMap(Collection::stream)
         .collect(Collectors.toList());
 
+    if (droppingDdls.isEmpty()) {
+      return;
+    }
+
     spannerDatabaseAdminTemplate.executeDdlStrings(droppingDdls, false);
   }
 
-  private Set<String> getInterleavedTableNames() {
+  private Set<Class<?>> getInterleavedEntityClasses() {
     return spannerMappingContext.getPersistentEntities().stream()
-        .flatMap(this::getChildEntities)
-        .map(SpannerPersistentEntity::tableName)
+        .flatMap(this::getInterleavedEntities)
+        .map(SpannerPersistentEntity::getType)
         .collect(Collectors.toSet());
   }
 
-  private Stream<SpannerPersistentEntity<?>> getChildEntities(
+  private Stream<SpannerPersistentEntity<?>> getInterleavedEntities(
       SpannerPersistentEntity<?> parentEntity) {
 
     return StreamSupport
         .stream(parentEntity.getPersistentProperties(Interleaved.class).spliterator(), false)
+        .map(PersistentProperty::getActualType)
+        .map(spannerMappingContext::getPersistentEntity);
+  }
+
+  private Set<Class<?>> getEmbeddedEntityClasses() {
+    return spannerMappingContext.getPersistentEntities().stream()
+        .flatMap(this::getEmbeddedEntities)
+        .map(SpannerPersistentEntity::getType)
+        .collect(Collectors.toSet());
+  }
+
+  private Stream<SpannerPersistentEntity<?>> getEmbeddedEntities(
+      SpannerPersistentEntity<?> entity) {
+
+    return StreamSupport
+        .stream(entity.getPersistentProperties(Embedded.class).spliterator(), false)
         .map(PersistentProperty::getActualType)
         .map(spannerMappingContext::getPersistentEntity);
   }
